28785a431235f9cf4051dac75d26e186894a2287
Donovan Thompson <donovan@nmteck.net>
2013-04-24 13:25:30 -0400

    QSR code
    Added qsr code to layout

--- js/main.js
+++ js/main.js
@@ -116,7 +116,9 @@
             this.hideTimeout = setTimeout(function(obj) { obj.hide(); }, timerSecs*1000, this);
         },
 		renderGoalMet: function(rewardText) {
-            adSlider.html("<div class='hackathon-ad'>Congratulations, davidpean, you earned the reward - " + rewardText + "<br><button class='adbutton' onclick='return false;'>Click here to claim</button><br><div>Found <b>2</b> <a href=''>Locations nearby</a></div></div>");
+            adSlider.html("<div class='hackathon-ad'><img id='hackathon-qsrcode' src='images/qsrcode.png' width='120' /> Congratulations, davidpean, you earned the reward - " + rewardText 
+                + "<br><button class='adbutton' onclick='return false;'>Click here to claim</button><br>"
+                + "<div>Found <b>2</b> <a href=''>Locations nearby</a></div></div>");
 			this.cancelPendingHide();
             this.show();
 		},
